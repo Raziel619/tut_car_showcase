c2e2fb9650e40b4f4d6353d35895fde76641dca9
Govinda Hosein <gh604721@digicelgroup.com>
2023-06-14 15:47:40 -0400

    Add public files

---
+++ components/Footer.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer className="flex flex-col mt-5 border-t border-gray-100 text-black-100">
+      <div className="flex flex-wrap justify-between gap-5 px-6 py-10 max-md:flex-col sm:px-16">
+        div.flex.flex-col.justify-start.ite
+      </div>
+
+    </footer>
+  )
+}
+
+export default Footer
--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,10 @@
 import CustomButton from './CustomButton';
+import Footer from './Footer';
 import Hero from './Hero';
+import Navbar from './Navbar';
 
 export {
-    Hero
+    Footer,
+    Hero,
+    Navbar
 };
