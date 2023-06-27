e4b448a83f787f94d8471a73682282e65c669522
Govinda Hosein <gh604721@digicelgroup.com>
2023-06-27 10:26:22 -0400

    Add CarCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-import { Hero, SearchBar, CustomFilter } from '@/components'
-import Image from 'next/image'
+import { CarCard, Hero, SearchBar, CustomFilter } from "@/components";
+import { fetchCars } from "@/utils";
+import Image from "next/image";
 
+export default async function Home() {
+  const allCars = await fetchCars();
 
-export default function Home() {
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden ">
-
       <Hero />
 
       <div className="mt-12 padding-x padding-y max-width" id="discover">
@@ -22,8 +25,22 @@ export default function Home() {
             <CustomFilter title="year" />
           </div>
         </div>
-      </div>
 
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div classname="home__error-container">
+            <h2 className="text-xl font-bold text-black">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
+      </div>
     </main>
-  )
+  );
 }
--- components/index.ts
+++ components/index.ts
@@ -1,17 +1,21 @@
-import CustomButton from './CustomButton';
-import CustomFilter from './CustomFilter';
-import Footer from './Footer';
-import Hero from './Hero';
-import Navbar from './Navbar';
-import SearchBar from './SearchBar';
-import SearchManufacturer from './SearchManufacturer';
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
 
 export {
-    CustomButton,
-    CustomFilter,
-    Footer,
-    Hero,
-    Navbar,
-    SearchBar,
-    SearchManufacturer
+  CarCard,
+  CarDetails,
+  CustomButton,
+  CustomFilter,
+  Footer,
+  Hero,
+  Navbar,
+  SearchBar,
+  SearchManufacturer,
 };
