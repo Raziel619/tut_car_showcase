080cc97601d2ddb9f505d5474eeb96d5390b1502
Govinda Hosein <gh604721@digicelgroup.com>
2023-06-27 10:48:47 -0400

    Add CarDetails transition

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -22,9 +22,29 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
             enterFrom="opacity-0"
             enterTo="opacity-100"
             leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
             <div className="fixed inset-0 bg-black bg-opacity-25"></div>
           </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex items-center justify-center min-h-full p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel>
+                  <button type="button" onClick={closeModal}></button>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
         </Dialog>
       </Transition>
     </>
